26dc758426b17fc4fb3f05f586792e10c52bd60a
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2023-03-16 14:43:05 -0300

    first commit

---
+++ teste.dart
@@ -0,0 +1,19 @@
+void main() {
+  // introducao a variaveis
+
+  String variavelNome = "Vanessa";
+  print(variavelNome);
+
+  int valorVariavel = 10;
+  print(valorVariavel);
+
+  bool ehVerdadeiro = true;
+  print(ehVerdadeiro);
+
+  List<String> listaDePalavras = ["Vanessa", "Van"];
+  print(listaDePalavras[1]);
+  print('${listaDePalavras[0]} - ${listaDePalavras[1]}');
+
+  // introducao null-safety
+  
+}
