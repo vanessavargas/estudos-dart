5514101f50dc056db4bad1211d1c33d67b15fa54
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2023-03-16 15:10:53 -0300

    add file estruturas-repeticao

---
+++ 4-estruturas-repeticao.dart
@@ -0,0 +1,12 @@
+void main() {
+  for (int i = 1; i <= 10; i++){
+    print(i * 2);
+  }
+
+  int contador = 10;
+
+  while (contador != 1) {
+    contador =  contador -1;
+    print("Loop -> $contador");
+  }
+}
