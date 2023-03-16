64ed2f92244e1c0b288e5bb30d6cabaa0cf8c0ec
Vanessa Vaegas <vanessa.vargas4f@gmail.com>
2023-03-16 14:58:59 -0300

    add if-switch

---
+++ 3-if-switch.dart
@@ -0,0 +1,38 @@
+void main() {
+  // if/else
+  bool seguirEmFrente = false;
+
+  if (seguirEmFrente) {
+    print("Andar");
+  } else {
+    print("Parar");
+  }
+
+  if(10 > 5) {
+    print("10 eh maior que 5");
+  }
+
+  //switch
+  int valorInt = 2;
+  
+  switch(valorInt) {
+    case 0:
+      print("ZERO");
+      break;
+    case 1:
+      print("UM");
+      break;
+    case 2:
+      print("DOIS");
+      break;
+    case 3:
+      print("TRÊS");
+      break;
+    case 4:
+      print("QUATRO");
+      break;
+    default:
+      print("QUATRO");
+      break;
+  }
+}
